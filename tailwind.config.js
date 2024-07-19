bef5895bbf5fa30b4916dc64a1c6f9ac9e865623
eustachekamala <eustachejared25@gmail.com>
2024-07-19 03:29:41 +0300

    I created the folder build that can held us to host out website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-    "./index.html",
-    "./index.js",
-    "style.css",
-  ],
+  content: ["./build/*html","./build/js/*.js"],
   theme: {
     extend: {},
   },
